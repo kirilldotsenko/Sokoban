37c8ebd71eaf41253d490f19d9b809b311095c5a
kirill <kirill@10.0.2.15>
2019-04-16 02:34:32 +0300

    New

--- SokobanNew/cmake-build-debug/MapRecipient.h
+++ SokobanNew/cmake-build-debug/MapRecipient.h
@@ -27,6 +27,24 @@ int check=0;
 char pl1;
 char pl2;
 char button;
+const TCODColor player{200, 200, 200};
+const TCODColor wall{0, 255, 0};
+const TCODColor boxChar{144, 238, 144};
+const TCODColor boxInt{128, 128, 128};
+const TCODColor box{255,255,0};
+const TCODColor markChar{144, 238, 144};
+const TCODColor markInt{128, 128, 128};
+const TCODColor mark{255,255,0};
+const TCODColor win{255,0,0};
+const vector<TCODColor> colourVec = {player, wall, boxChar, boxInt,box,markChar,markInt,mark,win};
+vector<int> Player;
+vector<int> Player2;
+map<Box <char>,vector <int> >O_BoxChar;
+map<Box <int>,vector <int> >O_BoxInt;
+map<CommonBox,vector<int>>O_Box;
+map<Mark <char>,vector <int> >X_MarkChar;
+map<Mark <int>,vector <int> >X_MarkInt;
+map<CommonMark,vector<int>>X_Mark;
 
 void MapR(){
     int sock;
@@ -89,24 +107,6 @@ void MapR(){
         TwoD<char> TestBoard;
         in >> TestBoard;
         TCODConsole::initRoot(TestBoard.GetDmX(), TestBoard.GetDmY(), "SOKOBAN");
-        const TCODColor player{200, 200, 200};
-        const TCODColor wall{0, 255, 0};
-        const TCODColor boxChar{144, 238, 144};
-        const TCODColor boxInt{128, 128, 128};
-        const TCODColor box{255,255,0};
-        const TCODColor markChar{144, 238, 144};
-        const TCODColor markInt{128, 128, 128};
-        const TCODColor mark{255,255,0};
-        const TCODColor win{255,0,0};
-        const vector<TCODColor> colourVec = {player, wall, boxChar, boxInt,box,markChar,markInt,mark,win};
-        vector<int> Player;
-        vector<int> Player2;
-        map<Box <char>,vector <int> >O_BoxChar;
-        map<Box <int>,vector <int> >O_BoxInt;
-        map<CommonBox,vector<int>>O_Box;
-        map<Mark <char>,vector <int> >X_MarkChar;
-        map<Mark <int>,vector <int> >X_MarkInt;
-        map<CommonMark,vector<int>>X_Mark;
         TestBoard.GameMap(TestBoard, colourVec, Player,Player2, X_MarkChar,X_MarkInt, O_BoxChar,O_BoxInt,TypeBox,X_Mark,O_Box,pl1,pl2);
         while (TestBoard.ifWin(X_MarkChar,X_MarkInt,X_Mark,counter) && EndOfGame(0)) {
             counter = updateWinPositions(X_MarkChar, X_MarkInt, O_BoxChar, O_BoxInt, O_Box, X_Mark, colourVec, pl1,
